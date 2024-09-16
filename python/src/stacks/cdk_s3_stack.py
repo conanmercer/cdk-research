7482f314667c428300f3a7c66ebfe82f25297d17
conanmercer <conan.mercer@sage.com>
2024-09-16 14:00:36 +0200

    add, l3 construct example

--- python/src/stacks/cdk_s3_stack.py
+++ python/src/stacks/cdk_s3_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import Stack
+from aws_cdk import Stack, Duration
 from constructs import Construct
 from aws_cdk import aws_s3 as s3
 
@@ -22,4 +22,26 @@ class Cdks3Stack(Stack):
             "MyL2Bucket",
             bucket_name="my-l2-bucket",
             versioned=True,  # L2 bucket makes enabling versioning simple
+        )
+
+        # L3 construct - A highly configured S3 bucket (using lifecycle rules, encryption, logging)
+        l3_bucket = s3.Bucket(
+            self,
+            "MyL3Bucket",
+            bucket_name="my-l3-bucket",
+            versioned=True,
+            encryption=s3.BucketEncryption.S3_MANAGED,  # Enable S3-managed encryption
+            lifecycle_rules=[
+                s3.LifecycleRule(
+                    id="MoveToInfrequentAccess",
+                    transitions=[
+                        s3.Transition(
+                            storage_class=s3.StorageClass.INFREQUENT_ACCESS,
+                            transition_after=Duration.days(30),
+                        ),
+                    ],
+                    expiration=Duration.days(365),
+                )
+            ],
+            server_access_logs_bucket=l2_bucket,  # Enable access logging to another bucket
         )
